1c71f1833bdd086081019e9bfb663c7ad3327143
Chinmaya Sahoo <er.compiler@gmail.com>
2020-05-13 10:05:36 +0530

    Changes to dwm config to refresh status when volume changes.

--- .local/bin/config.h
+++ .local/bin/config.h
@@ -2,8 +2,9 @@
 
 #include <X11/XF86keysym.h>
 
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+//static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",  NULL };
+static const char *upvol[]   = { "bash", "volume", "+5%",  NULL };
+static const char *downvol[] = { "bash", "volume", "-5%",  NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 //static const char *hibernate[] = { "sudo", "systemctl", "suspend", NULL };
 static const char *hibernate[] = { "bash", "hibernet", NULL };
